d647cdfd1ffe82786936528d2a7564bd0e3bc6ec
m-massoud <mohamed97masoud97@gmail.com>
2025-10-17 15:58:42 +0300

    fix running project error

--- src/plugins/vee-validate.js
+++ src/plugins/vee-validate.js
@@ -1,6 +1,5 @@
-import { defineRule , configure } from 'vee-validate';
-import AllRules from '@vee-validate/rules';
-
+import { defineRule, configure } from "vee-validate";
+import * as AllRules from "@vee-validate/rules";
 
 // const customMessages = {
 //     getMessage(_,value){
@@ -18,26 +17,29 @@ import AllRules from '@vee-validate/rules';
 //         })
 //     }
 // }
-Object.keys(AllRules).forEach(rule => {
+Object.keys(AllRules).forEach((rule) => {
+  if (typeof AllRules[rule] === "function") {
     defineRule(rule, AllRules[rule]);
-})
+  }
+});
 
 configure({
-    generateMessage: (ctx) => {
-        const messages = {
-            required: `The ${ctx.field} field is required`,
-            email: `The ${ctx.field} field must be a valid email`,
-            min: `The ${ctx.field} field must be at least ${ctx.rule.params} characters`,
-            max: `The ${ctx.field} field must be at most ${ctx.rule.params} characters`,
-            confirmed: `The ${ctx.field} field confirmation does not match`,
-            min_value: `The ${ctx.field} field must be greater than ${ctx.min}`,
-            max_value: `The ${ctx.field} field must be less than ${ctx.max}`,
-            length: `The ${ctx.field} field must be ${ctx.length} characters`,
-        }
-        return messages[ctx.rule.name] ? messages[ctx.rule.name] : `The ${ctx.field} field is invalid`
-    }
-})
-
+  generateMessage: (ctx) => {
+    const messages = {
+      required: `The ${ctx.field} field is required`,
+      email: `The ${ctx.field} field must be a valid email`,
+      min: `The ${ctx.field} field must be at least ${ctx.rule.params} characters`,
+      max: `The ${ctx.field} field must be at most ${ctx.rule.params} characters`,
+      confirmed: `The ${ctx.field} field confirmation does not match`,
+      min_value: `The ${ctx.field} field must be greater than ${ctx.min}`,
+      max_value: `The ${ctx.field} field must be less than ${ctx.max}`,
+      length: `The ${ctx.field} field must be ${ctx.length} characters`,
+    };
+    return messages[ctx.rule.name]
+      ? messages[ctx.rule.name]
+      : `The ${ctx.field} field is invalid`;
+  },
+});
 
 // For localization
 // configure({
